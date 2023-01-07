92017f53b9d5e6af7fee50479047f3a9fb603c6e
Zubair-05 <mullamdz0501@gmail.com>
2023-01-07 09:58:52 +0530

    changed my favourite food

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <h1>Studying B.Tech in Civil Engineering at NITW</h1>
         <h1>But I'm interested in Software Engieering</h1>
         <h1>I'm from Kanigiri</h1>
-        <h1>I love paratha</h1>
+        <h1>I love Poori</h1>
         <h1>My best freind in Myself</h1>
         <h1>abcd</h1>
         <img src={heroImage}/>
