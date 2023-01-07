5d9894ffd988dd8336efb117f67bd578d5848acf
Zubair-05 <mullamdz0501@gmail.com>
2023-01-07 09:39:51 +0530

    Add by best friend

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <h1>But I'm interested in Software Engieering</h1>
         <h1>I'm from Kanigiri</h1>
         <h1>I love paratha</h1>
+        <h1>My best freind in Myself</h1>
       </header>
     </div>
   );
