ce9cf6601729339cb386bdbedeb063504d9b2d42
Zubair-05 <mullamdz0501@gmail.com>
2023-01-07 09:38:01 +0530

    Add my favourite food

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <h1>Studying B.Tech in Civil Engineering at NITW</h1>
         <h1>But I'm interested in Software Engieering</h1>
         <h1>I'm from Kanigiri</h1>
+        <h1>I love paratha</h1>
       </header>
     </div>
   );
